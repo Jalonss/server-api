4847a66827c1eabb579c6704ecc8c103287a859d
Jalonss <alonss739@gmail.com>
2022-10-10 20:53:50 -0600

    Use enviroment variable

--- funcitons/items.js
+++ funcitons/items.js
@@ -3,21 +3,16 @@ import ItemTransform from "../utils/_InterfaceItems.js";
 import ItemSpecsTransform from "../utils/_InterfaceItemSpecs.js";
 
 async function getItems(req) {
-    return fetch(`https://api.mercadolibre.com/sites/MLA/search?limit=4&q=${req.q}`)
+    return fetch(`${process.env.API_URL_SEARCH_ITEMS}?limit=4&q=${req.q}`)
         .then(res => res.json())
-        .then(json => ResponseJSON(ItemTransform(json), 200, "Get Item by Q"))
+        .then(json => ResponseJSON(ItemTransform(json), 200, "Get Item by Query"))
 }
 
 async function getItemsSpecs(req) {
     return Promise.all([
-        fetch(`https://api.mercadolibre.com/items/${req.id}`).then(res => res.json()),
-        fetch(`https://api.mercadolibre.com/items/${req.id}/description`).then(res => res.json()),
+        fetch(`${process.env.API_URL_ITEM}/${req.id}`).then(res => res.json()),
+        fetch(`${process.env.API_URL_ITEM}/${req.id}/description`).then(res => res.json()),
     ]).then(([specs, description]) => ResponseJSON(ItemSpecsTransform({ specs, description: description.plain_text }), 200, "Get Item by Id"))
-}
-async function getItemsDescription(id) {
-    return fetch(`https://api.mercadolibre.com/items/${id}/description`)
-        .then(res => res.json())
-        .then(json => ResponseJSON(json, 200, "Get Description by Id"))
 }
 
 export { getItems, getItemsSpecs };
